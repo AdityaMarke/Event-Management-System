abe2a7df1e1b666353b39364d111c4da43cd1e02
Aditya Marke <156222518+AdityaMarke@users.noreply.github.com>
2025-12-06 11:01:17 +0530

    Delete add_unique_index.sql

--- add_unique_index.sql
+++
@@ -1,6 +0,0 @@
--- Add unique constraint to prevent duplicate registrations
--- Run this SQL to add an index on event_id and email to prevent duplicates
-
-ALTER TABLE registrations 
-ADD UNIQUE INDEX idx_event_email (event_id, email);
-
